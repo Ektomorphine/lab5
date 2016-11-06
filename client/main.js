12cd26af53b6879106ba24299e20f60179b0ecd5
Ektomorphine <iglad96@gmail.com>
2016-11-07 02:49:43 +0300

    3

--- client/main.js
+++ client/main.js
@@ -1,9 +1,24 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
 
-Template.body.helpers({
-tasks: [
-{ text: 'This is task 1' },
-{ text: 'This is task 2' },
-{ text: 'This is task 3' },
-]
+import './main.html';
+
+Template.hello.onCreated(function helloOnCreated() {
+  // counter starts at 0
+  this.counter = new ReactiveVar(0);
 });
 
+Template.hello.helpers({
+  counter() {
+    return Template.instance().counter.get();
+  },
+});
+
+Template.hello.events({
+  'click button'(event, instance) {
+    // increment the counter when button is clicked
+    instance.counter.set(instance.counter.get() + 1);
+  },
+});
+
+import '../imports/ui/body.js';
